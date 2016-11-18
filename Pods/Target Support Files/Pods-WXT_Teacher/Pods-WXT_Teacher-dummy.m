8881e48e64fc6c27a1ef2e425035799065056088
zhangsiqiang07 <1328254616@qq.com>
2016-11-18 17:56:01 +0800

    pod

---
+++ Pods/Target Support Files/Pods-WXT_Teacher/Pods-WXT_Teacher-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WXT_Teacher : NSObject
+@end
+@implementation PodsDummy_Pods_WXT_Teacher
+@end
---
+++ Pods/Target Support Files/Pods-WXT_Teacher/Pods-WXT_Teacher-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_WXT_TeacherVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_WXT_TeacherVersionString[];
+
